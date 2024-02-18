35f56fff51f2fc895889325d59b3d92fd91af0ca
INwafor <inwafor@uoguelph.ca>
2024-02-17 19:22:34 -0500

    organizing files

--- code/02-Rstar-Phosphorus.R
+++ code/02-Rstar-Phosphorus.R
@@ -1,3 +1,8 @@
 ##Feb13th 
 ##worrk on second experiment trial 
 # Rstar for Phosphate
+
+##plan: start by making each of the sheets into a data frame - join those all together - thats one file of code
+## next file make a path from this code and organize/clean up the data frame by species, make times/dates correct
+## path from that code to make a new one that plots the R* by temperature and use that to analyze 
+## finally one last script to work on the monod curves and fitting the data to growth models
