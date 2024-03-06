93d972c82a3e9eb14fbc921464ad0a52e9666b18
INwafor <inwafor@uoguelph.ca>
2024-03-06 00:16:02 -0500

    save edits + created new script with notes on analyses

--- code/05_phosphate_monod.R
+++ code/05_phosphate_monod.R
@@ -1,3 +1,4 @@
 ## going to work on monod curves 
 source("code/03_RFUS_fist.R")
 source("code/04_RFUS_scen.R")
+
---
+++ code/06_analysis_flow.R
@@ -0,0 +1,15 @@
+## going through Arya's analysis help map 
+
+#two sample - sampled paired 
+#-2 one sample t-test and compare } treat set of differences as one sample 
+
+#two sample - samples independent 
+# - two sample t-test with pooled variance term? (normal distribution, normal variance)
+# - welch two sample t-test, no pooled variances (normal distribution, unequal variances)
+# - mann-whitney or wilcoxon signed rank test (very non normally distributed, but similar distribution shapes/variances)
+# - transformation / reasses / descriptive statistics (one/both very non normally distributed, without similated variances)
+
+#can we do more than 2 samples with the mixed plate? wouldnt make any sense
+## one predictor variable (r* or treatment - categorical?) ( or more than?) - categorical or continuous?
+## - one variable (categorical): some type of ANOVA if there is equal or unequal variance, Kruskal wallis for non normal distribution and similar variance
+## - multiple categorical == two factor anova
