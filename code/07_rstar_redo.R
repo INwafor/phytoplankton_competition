6b217375094ff27e50d05cf7cf80ec4818eee40d
INwafor <inwafor@uoguelph.ca>
2024-03-15 14:14:20 -0400

    finished graphs

--- code/07_rstar_redo.R
+++ code/07_rstar_redo.R
@@ -19,7 +19,7 @@ plate_layout <- plate_layout[!(row.names(plate_layout) %in% c("1")),]
 
 RFU_files2 <- c(list.files("data/rstar2_dataraw", full.names = TRUE))
 
-RFU_files2 <- RFU_files2[grepl(".xls", RFU_files)]
+RFU_files2 <- RFU_files2[grepl(".xls", RFU_files2)]
 
 names(RFU_files2) <- RFU_files2 %>% 
   gsub(pattern = ".xlsx$", replacement = "") %>% 
@@ -27,9 +27,7 @@ names(RFU_files2) <- RFU_files2 %>%
 
 all_plates <- map_df(RFU_files2, read_excel, range = "A15:M23", .id = "file_name")%>%
   rename(row = ...1) %>% 
-  mutate(file_name = str_replace(file_name, " ", ""))
-
-%>%
+  mutate(file_name = str_replace(file_name, " ", ""))%>%
   separate(file_name, into = c("data", "location", "loc_2", "file_name", "temp", "read"), remove = FALSE)%>%
   select(-data,
          -location,
@@ -38,24 +36,24 @@ all_plates <- map_df(RFU_files2, read_excel, range = "A15:M23", .id = "file_name
 all_plates <- unite(all_plates, file_name, c(file_name, temp, read))
 view(all_plates)
 
-all_times <- map_df(RFU_files, read_excel, range = "A7:A8", .id = "file_name") %>% 
+all_times <- map_df(RFU_files2, read_excel, range = "A7:A8", .id = "file_name") %>% 
   clean_names()
 
 all_times <- all_times %>%
-  rename("Day 1" = "date_2_14_2024",
-         "Day 2" = "date_2_15_2024",
-         "Day 3" = "date_2_16_2024",
-         "Day 4" = "date_2_17_2024")
+  rename("Day 0" = "date_3_11_2024",
+         "Day 1" = "date_3_12_2024",
+         "Day 2" = "date_3_13_2024",
+         "Day 3" = "date_3_14_2024")
 
 all_times <- all_times %>% separate(file_name, into = c("data", "location", "loc_2", "file_name", "temp", "read"), remove = FALSE) %>%
   select(-data,
          -location,
          -loc_2)
 
+all_times$`Day 0` <- gsub("Time: ", "", as.character(all_times$`Day 0`))
 all_times$`Day 1` <- gsub("Time: ", "", as.character(all_times$`Day 1`))
 all_times$`Day 2` <- gsub("Time: ", "", as.character(all_times$`Day 2`))
 all_times$`Day 3` <- gsub("Time: ", "", as.character(all_times$`Day 3`))
-all_times$`Day 4` <- gsub("Time: ", "", as.character(all_times$`Day 4`))
 
 all_times <- unite(all_times, file_name, c(file_name, temp, read))
 
@@ -63,8 +61,6 @@ all_plates2 <- dplyr::left_join(all_plates, all_times, by = "file_name")
 
 all_plates2 <- all_plates2 %>%
   mutate(row = as.character(row))
-
-view(all_plates2)
 
 ## saying that the column isnt present ? where i get stuck trying to add in my well key layout
 all_temp_RFU <- all_plates2 %>% 
@@ -102,59 +98,12 @@ all_temp_RFU <- subset(all_temp_RFU, select = -c(row, well))
 colnames(all_temp_RFU)[colnames(all_temp_RFU) == "well_key"] <- "well"
 
 ##adding in R concentration - now we can left bind with plate layout
-all_merged <- left_join(all_temp_RFU, plate_layout, by = "well")
-
-## Joeys code - not using for this script anymore 
-{
-  bind_cols(plate_layout_repeated[c("well", "treatment", "r_concentration")])
-  view(all_temp_RFU)
-  
-  str(all_plates2)
-  
-  str(plate_layout_repeated)
-  
-  ##if number 1 = a - h t
-  
-  unite(all_plates2, col = "well", remove = FALSE, sep = "") 
-  mutate(column = formatC(column, width = 2, flag = 0)) %>% 
-    mutate(column = str_replace(column, " ", "0")) %>% 
-    unite(col = well, row, column, sep = "") %>% 
-    filter(!is.na(RFU))
-  
-  
-  
-  all_rfus_raw <- left_join(all_temp_RFU, plate_info, by = c("well"))
-  
-  
-  
-  all_rfus2 <- all_rfus_raw %>%
-    unite(col = date_time, Date, time, sep = " ") %>%
-    mutate(date_time = ymd_hms(date_time)) %>% 
-    mutate(population = ifelse(population == "cc1629", "COMBO", population))
-  
-  
-  all_rfus3 <- all_rfus2 %>% 
-    group_by(n_level) %>% 
-    mutate(start_time = min(date_time)) %>% 
-    mutate(days = interval(start_time, date_time)/ddays(1)) %>% 
-    unite(col = well_plate, well, plate, remove =  FALSE) %>% 
-    separate(col = n_level, sep = 1, into = c("p", "phosphate_level")) %>% 
-    mutate(phosphate_level = as.numeric(phosphate_level))
-}
-
-##Cleaning Up
-merged_time <- all_merged %>% 
+march15_rfus <- left_join(all_temp_RFU, plate_layout, by = "well") %>%
   clean_names()
-view(merged_time)
 
-##using file_name - create a new df with times/ file 
-# attach hour thing to file name ? maybe have to separate both dataframes 
-# and then based off what read it is - assign the times to it in a new column?
 file_hours <- all_times %>%
   clean_names() %>%
   separate(file_name, into = c("spec", "temp", "read"), remove = FALSE)
-
-view(file_hours)
 
 file_hours_new <- file_hours %>%
   mutate(
@@ -176,87 +125,131 @@ file_hours_new <- file_hours %>%
 ## merge file_name all back together
 all_times_cleaned <- unite(file_hours_new, spec_temp, c(spec, temp))
 
-view(all_times_cleaned)
-
 #now add this into the all_merged by day_1
-final_merge <- left_join(merged_time, all_times_cleaned, by = "file_name")
+march15_rfus_final <- left_join(march15_rfus, all_times_cleaned, by = "file_name")
 
-final_merge <- final_merge %>%
+march15_rfus_final <- march15_rfus_final %>%
   select(-day_1.x,
          -day_2.x,
          -day_3.x,
-         -day_4.x,
+         -day_0.x,
          -day_1.y,
          -day_2.y,
          -day_3.y,
-         -day_4.y,
+         -day_0.y,
          -file_name)
 
-view(final_merge)
+##plotting rfus for all three plates 
+library(viridis)
+library(ggsci)
+library(RColorBrewer)
+library(janitor)
+library(lubridate)
+library(broom)
+#Fist_21C
+Fist_21C <- march15_rfus_final %>%
+  filter(grepl("Fist_21C", spec_temp))
 
-## code below is for changing hours if you want/have to later on
-{
-  ##change time format  can try this again if you need to change up the hours. 
-  # mutate(days = Hour/24) %>% # not 
-  library(purrr)
-  
-  merged_time$day_1_POSIXct <- as.POSIXct(merged_time$day_1, format = "%I:%M:%S %p")
-  
-  # Format to "%H:%M:%S %p" and store in another column
-  merged_time$day_1_formatted <- format(merged_time$day_1_POSIXct, format = "%H:%M:%S")
-  
-  time_to_numeric <- function(time_string) {
-    parts <- strsplit(time_string, ":")[[1]]
-    hours <- as.numeric(parts[1])
-    minutes <- as.numeric(parts[2])
-    seconds <- as.numeric(parts[3])
-    total_seconds <- hours * 3600 + minutes * 60 + seconds
-    return(total_seconds)
-  }
-  
-  merged_time$day_1_numeric <- sapply(merged_time$day_1_formatted, time_to_numeric)
-  
-  view(merged_time)
-  
-  
-  
-  merged_time$day_1 <- format(merged_time$day_1, format = "%H:%M:%S %p")
-  merged_time$day_2 <- format(merged_time$day_2, format = "%H:%M:%S %p")
-  merged_time$day_3 <- format(merged_time$day_3, format = "%H:%M:%S %p")
-  merged_time$day_4 <- format(merged_time$day_4, format = "%H:%M:%S %p")
-  
-  view(merged_time)
-  
-  str(merged_time)
-  print(merged_time[, c("day_1", "day_2", "day_3", "day_4")])
-  
-  ##make time increasing - this didnt work 
-  merged_hours <- merged_time %>%
-    mutate(
-      time_sequence = pmap_dbl(select(., starts_with("day")), ~ as.numeric(difftime(..1, ..[1], units = "hours")))
-    )
-  
-  ## only changed day 1, added todays date 
-  for (i in 2:5) {
-    all_merged[[i]] <- as.POSIXct(all_merged[[i]], format = "%H:%M:%S %p")
-    
-    print(sapply(all_merged[2:5], class))
-  } 
-  view(all_merged)
-  
-  # View the resulting data frame
-  print(data)
-  
-  all_times_merged <- all_merged %>%
-    mutate(`Day 1` = as.numeric(`Day 1`))
-  
-  str(all_times_merged)
-  
-  
-  unite(col = date_time, Date, time, sep = " ") %>%
-    mutate(date_time = ymd_hms(date_time)) %>% 
-    
-    mutate(start_time = min(date_time)) %>% 
-    mutate(days = interval(start_time, date_time)/ddays(1))
-}
+Fist_21C <- Fist_21C %>%
+  mutate(r_concentration = factor(r_concentration))
 
+Fist_21C %>% 
+  filter(r_concentration != 0) %>% 
+  ggplot(aes(x = day, y = log(rfu), group = well, color = r_concentration)) + 
+  geom_point(col = "black", shape = 1, alpha = 0.6) + 
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_wrap(~r_concentration, scales = "free_y")+ scale_color_npg()+
+  ylab("log(rfu)") + xlab("day") +
+  ggtitle("logged Fist_21C r*")
+#ggsave("figures/fist_21C_logged", width = 15, height = 10)
+
+Fist_21C_growth <- Fist_21C %>% 
+  filter(treatment != "Blank") %>% 
+  group_by(well) %>% 
+  do(tidy(lm(log(rfu) ~ day, data = .))) 
+
+##filter out (intercept)
+Fist_21C_growth2 <- Fist_21C_growth %>% 
+  left_join(plate_layout) %>%
+  filter(term == "day")
+
+concentration_order <- c("0.5", "1", "2", "4", "6", "8", "10", "20", "35", "50")
+
+Fist_21C_growth2 %>% 
+  filter(term == "day") %>% 
+  mutate(r_concentration = factor(r_concentration, levels = concentration_order)) %>%
+  ggplot(aes(x = r_concentration, y = estimate)) + geom_point() +
+  ylab("growth rate (per day)") + xlab("resource level") +
+  ggtitle ("growth rate - Fist 21C")
+
+#Fist_30C
+Fist_30C <- march15_rfus_final %>%
+  filter(grepl("Fist_30C", spec_temp))
+
+Fist_30C <- Fist_30C %>%
+  mutate(r_concentration = factor(r_concentration))
+
+Fist_30C %>% 
+  filter(r_concentration != 0) %>% 
+  ggplot(aes(x = day, y = log(rfu), group = well, color = r_concentration)) + 
+  geom_point(col = "black", shape = 1, alpha = 0.6) + 
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_wrap(~r_concentration, scales = "free_y")+ scale_color_npg()+
+  ylab("log(rfu)") + xlab("day") +
+  ggtitle("logged Fist_30C r*")
+#ggsave("figures/fist_30C_logged", width = 15, height = 10)
+
+Fist_30C_growth <- Fist_30C %>% 
+  filter(treatment != "Blank") %>% 
+  group_by(well) %>% 
+  do(tidy(lm(log(rfu) ~ day, data = .))) 
+
+##filter out (intercept)
+Fist_30C_growth2 <- Fist_30C_growth %>% 
+  left_join(plate_layout) %>%
+  filter(term == "day")
+
+concentration_order <- c("0.5", "1", "2", "4", "6", "8", "10", "20", "35", "50")
+
+Fist_30C_growth2 %>% 
+  filter(term == "day") %>% 
+  mutate(r_concentration = factor(r_concentration, levels = concentration_order)) %>%
+  ggplot(aes(x = r_concentration, y = estimate)) + geom_point() +
+  ylab("growth rate (per day)") + xlab("resource level") +
+  ggtitle ("growth rate - Fist 30C")
+
+#Scen_8C
+Scen_8C <- march15_rfus_final %>%
+  filter(grepl("Scen_8C", spec_temp))
+
+Scen_8C <- Scen_8C %>%
+  mutate(r_concentration = factor(r_concentration))
+
+Scen_8C %>% 
+  filter(r_concentration != 0) %>% 
+  ggplot(aes(x = day, y = log(rfu), group = well, color = r_concentration)) + 
+  geom_point(col = "black", shape = 1, alpha = 0.6) + 
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_wrap(~r_concentration, scales = "free_y")+ scale_color_npg()+
+  ylab("log(rfu)") + xlab("day") +
+  ggtitle("logged Scen_8C r*")
+#ggsave("figures/Scen_8C_logged", width = 15, height = 10)
+
+Scen_8C_growth <- Scen_8C %>% 
+  filter(treatment != "Blank") %>% 
+  group_by(well) %>% 
+  do(tidy(lm(log(rfu) ~ day, data = .))) 
+
+##filter out (intercept)
+Scen_8C_growth2 <- Scen_8C_growth %>% 
+  left_join(plate_layout) %>%
+  filter(term == "day")
+
+concentration_order <- c("0.5", "1", "2", "4", "6", "8", "10", "20", "35", "50")
+
+Scen_8C_growth2 %>% 
+  filter(term == "day") %>% 
+  mutate(r_concentration = factor(r_concentration, levels = concentration_order)) %>%
+  ggplot(aes(x = r_concentration, y = estimate)) + geom_point() +
+  ylab("growth rate (per day)") + xlab("resource level") +
+  ggtitle ("growth rate - Scen_8C")
