21d6d42c5af28e48d20ae606e8a40404783cfc83
INwafor <inwafor@uoguelph.ca>
2024-03-25 19:34:40 -0400

    up 2 date

--- code/08_comp.R
+++ code/08_comp.R
@@ -10,6 +10,15 @@ library(janitor)
 library(lubridate)
 
 comp <- read_excel("data/comp_phycoprobe.xlsx") %>% 
-  clean_names() 
+  clean_names() %>%
+  rename("green_algae_ug" = green_algae_4,
+         "green_algae_ml" = green_algae_5,
+         "diatom_ug" = diatoms_6,
+         "diatom_ml" = diatoms_7,
+         "tot_conc_ug" = total_conc,
+         "tot_density_ml" = total_cell_count)
 
 comp <- comp[!(row.names(comp) %in% c("1")),]
+
+##average each replicate reads - so then we have 1 "read" per then for all the cold / hot
+
