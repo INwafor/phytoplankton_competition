b1cd5cb8038aa218a67f1ee115225b0bc9e58205
INwafor <inwafor@uoguelph.ca>
2024-03-27 17:48:17 -0400

    monod curves still looking weird what to do

--- code/05_monod_1.R
+++ code/05_monod_1.R
@@ -33,13 +33,15 @@ phosphate_exp <- all_merged %>%
 ## what is NO rfu
 
 ##estimate growth rates first then fit the monod curve OR direct method (estimate parameters directly)
-#plot edtimate as function of r_concentration
+#plot estimate as function of r_concentration
+##how to lay out all the concentration levels on the plot?
+
 ##FEBRUARY DATA 
 #Fist 21C
 Fist_21C_growth2 %>% 
   ggplot(aes(x = r_concentration, y = estimate)) + 
   geom_point(col = "black", shape = 1, alpha = 0.6)
-## estimate didnt remove the intercept fix that
+##something concentration order - values chr 
 
 mod1 <- nls(estimate ~ umax* (r_concentration / (ks+ r_concentration)),
               data= Scen_21C_growth2,  start=list(ks = 1, umax = 1), algorithm="port",
@@ -55,10 +57,8 @@ preds <- augment(nls(estimate ~ umax* (r_concentration/ (ks+ r_concentration)),
 
 preds <- augment(mod1)
 
-##stuff down here isnt working too good 
-{
-
-Scen_21C_growth2 %>% 
+## the preds model puts it too high up - the geom line - how can we fix - also show concentration values
+Fist_21C_growth2 %>% 
   mutate(r_concentration = as.numeric(r_concentration)) %>%
   ggplot(aes(x= r_concentration, y= estimate)) + geom_point() +
   # geom_errorbar(aes(ymin=estimate-best.se, ymax=estimate + best.se), width=.2) + 
@@ -69,22 +69,22 @@ Scen_21C_growth2 %>%
 
 growth_rates <- phosphate_exp %>%
   group_by(r_concentration) %>%
-  do(tidy(nls(rfu ~ N0 * exp(r*day),
+  do(tidy(nls(RFU ~ N0 * exp(r*day),
               data= .,  start=list(r=0.01),
               control = nls.control(maxiter=100, minFactor=1/204800000)))) %>% 
   ungroup() 
 
+#growth2 <- left_join(growth_rates, treatments, by = "population") %>% 
+ # mutate(treatment = ifelse(is.na(treatment), "none", treatment))
 
-growth2 <- left_join(growth_rates, treatments, by = "population") %>% 
-  mutate(treatment = ifelse(is.na(treatment), "none", treatment))
-
-
-growth2 %>% 
-  mutate(nitrate_concentration = as.numeric(nitrate_concentration)) %>% 
-  ggplot(aes(x = nitrate_concentration, y = estimate)) + geom_point() +
-  facet_grid(treatment ~ ancestor_id) + geom_hline(yintercept = 0) + ylab("Exponential growth rate (/day)") +
+#the concentrations with growth rates over 0 - just need to tidy
+growth_rates %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) %>% 
+  ggplot(aes(x = r_concentration, y = estimate)) + geom_point() +
+  #facet_grid(treatment ~ ancestor_id) + 
+  geom_hline(yintercept = 0) + ylab("Exponential growth rate (/day)") +
   xlab("Nitrate concentration (uM)") 
-}
+
 
 # growth rates with growthTools -------------------------------------------
 library(growthTools)
@@ -92,42 +92,42 @@ library(growthTools)
 #will i not get proper growth rates? 
 
 ##Fist_21C only 
-str(final_merge)
+str(all_merged)
 
-b2 <- final_merge %>% 
+b2 <- all_merged %>% 
   filter(r_concentration == 0.5) %>% 
-  filter(spec_temp == "Fist_21C") %>% 
-  mutate(ln.fluor = log(rfu)) 
+  filter(file_name == "Fist_21C") %>% 
+  mutate(ln.fluor = log(RFU)) 
 
 ##one again points are just displayed on top of eachother (line connection is jagged)
-final_merge %>%
+all_merged %>%
   # filter(spec_temp == "Fist_21C") %>%
-  ggplot(aes(x = day, y = rfu, group = r_concentration, color = treatment)) + geom_point() + geom_line()
+  ggplot(aes(x = time_elapsed_units, y = RFU, group = r_concentration, color = treatment)) + geom_point() + geom_line()
 
 
 res <- get.growth.rate(b2$day, b2$ln.fluor, plot.best.Q =T, id = "Fist_21C")
 
 #literally what am i looking at 
-growth_rates_n <- final_merge %>%
-  filter(spec_temp != "Fist_21C") %>% 
-  mutate(ln.fluor = log(rfu)) %>% 
+growth_rates_p <- all_merged %>%
+  filter(file_name != "Fist_21C") %>% 
+  mutate(ln.fluor = log(RFU)) %>% 
   filter(r_concentration > 0.5) %>% 
-  do(grs=get.growth.rate(x=.$day, y=.$ln.fluor,id=.$spec_temp,plot.best.Q=T))
+  do(grs=get.growth.rate(x=.$day, y=.$ln.fluor,id=.$file_name,plot.best.Q=T))
 
 
-growth_sum_n <- growth_rates_n %>%
-  summarise(spec_temp,mu=grs$best.slope,
+growth_sum_p <- growth_rates_p %>%
+  summarise(file_name,mu=grs$best.slope,
             best.model=grs$best.model,best.se=grs$best.se)
 
-all_growth_n <- left_join(growth_sum_n, treatments5, by = c("well_plate")) %>% 
-  filter(!is.na(ancestor_id))
-write_csv(all_growth_n, "data-processed/exponential_growth_nitrate.csv")
+all_growth_p <- left_join(growth_sum_p, treatments5, by = c("plate_layout"))
 
-all_growth_n <- read_csv("data-processed/exponential_growth_nitrate.csv")
-all_growth_n %>% 
-  filter(!is.na(ancestor_id)) %>% 
-  mutate(nitrate_concentration = as.numeric(nitrate_concentration)) %>% 
-  ggplot(aes(x = nitrate_concentration, y = mu)) + geom_point() +
-  facet_grid(treatment ~ ancestor_id) + geom_hline(yintercept = 0) + ylab("Exponential growth rate (/day)") +
-  xlab("Nitrate concentration (uM)") 
+#write_csv(all_growth_n, "data-processed/exponential_growth_nitrate.csv")
+#all_growth_n <- read_csv("data-processed/exponential_growth_nitrate.csv")
+
+all_growth_p %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) %>% 
+  ggplot(aes(x = r_concentration, y = mu)) + geom_point() +
+  #facet_grid(treatment ~ ancestor_id) + 
+  geom_hline(yintercept = 0) + ylab("Exponential growth rate (/day)") +
+  xlab("Phosphate concentration (uM)") 
 
