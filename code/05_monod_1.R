423c9f125dba35b44d4f40218d51ea13c545ba40
INwafor <inwafor@uoguelph.ca>
2024-04-14 16:16:01 -0400

    final rfu graph

--- code/05_monod_1.R
+++ code/05_monod_1.R
@@ -45,18 +45,16 @@ rfu_df %>%
   geom_smooth(method = "loess", se = FALSE) +
   facet_wrap(file_name ~ r_concentration, scales = "free_y") + 
   scale_color_manual(
-    values = c("hotpink4", "#9e0142","#d53e4f","sienna1","#fbcf51", "#b6f598","#4bc425","#66c2a5","#3288bd","#5e4fa2","#d7a4dd")) +
+    values = c("hotpink4", "#9e0142","#d53e4f","sienna1","#fbcf51","#4bc425","#66c2a5","#3288bd","#5e4fa2","hotpink")) +
   ylab("") + 
   xlab("") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        strip.background = element_rect(size = 0.5),
-        strip.text = element_text(size = 8),
-        panel.spacing = unit(-0.2, "lines"),
-        legend.position = "none")
-+ 
-  ggsave("rfu_final3.png", width = 15, height = 10, dpi = 300)
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 9),
+        axis.text.y = element_text(size = 9),
+        strip.text = element_text(size = 8),         
+        panel.spacing = unit(1, "lines"),
+        panel.grid.major = element_line(color = "gray", linetype = "dotted"),
+        legend.position = "none") #+ ggsave("rfu_final4.png", width = 15, height = 10, dpi = 300)
 
-options(repr.plot.width = 15, repr.plot.height = 10)
 
 phosphate_exp <- rfu_df %>% 
   select(-read) %>%
