6be1d823a279624df438b6da1a98e4b0453b9124
INwafor <inwafor@uoguelph.ca>
2024-04-18 23:50:49 -0400

    fitting confidence intervals

--- code/09_bootstrapping_monod.R
+++ code/09_bootstrapping_monod.R
@@ -1,7 +1,23 @@
 #monod # 2
 source("code/05_monod_1.R")
 
+library(tidyverse)
+library(cowplot)
+library(broom)
+library(readxl)
+library(janitor)
+library(plotrix)
+library(here)
+library(boot)
+library(car)
+library(rTPC)
+library(nls.multstart)
+library(patchwork)
+library(minpack.lm)
+theme_set(theme_cowplot())
+
 ##starter monod curve - this is what i have rn and am attempting to bootstrap 
+## keep as is with negative growth rates too - estimates from monod curve unreliable for scen 30C
 preds4 %>%
   mutate(r_concentration = as.numeric(r_concentration)) %>%  
   ggplot(aes(x = r_concentration, y = estimate)) + 
@@ -13,35 +29,137 @@ preds4 %>%
   geom_errorbar(aes(ymin=estimate-best_se, ymax=estimate + best_se), width=.2) + 
   geom_line(data = preds4, aes(x = r_concentration, y = fitted), color = "#9e0142", size = 1) +
   ylab("Exponential growth rate (/day)") + 
-  xlab("Phosphate concentration (uM)") 
+  xlab("Phosphate concentration (uM)") +
+ # geom_ribbon(data = all_bootCI_df, aes(x = File_Name, ymin = CI.2.5., ymax = CI.97.5.), alpha = 0.3)
 
-## positive only - fist 8c, scen 21c
-preds_subset <- preds4 %>%
-  filter(file_name != "Fist_21C",
-         file_name != "Fist_30C",
-         file_name != "Scen_30C",
-         file_name != "Scen_8C")
-
-preds_subset %>%
-  mutate(r_concentration = as.numeric(r_concentration)) %>%  
-  ggplot(aes(x = r_concentration, y = estimate)) + 
-  geom_point() +
-  geom_smooth(method = "nls", formula = y ~ umax * (x / (ks + x)),
-              method.args = list(start = list(umax = 0.5, ks = 0.5)),
-              se = FALSE) +
-  facet_wrap(~ file_name, scales = "free") +
-  #geom_errorbar(aes(ymin=estimate-best_se, ymax=estimate + best_se), width=.2) + 
-  geom_line(data = preds_subset, aes(x = r_concentration, y = fitted), color = "#3288bd", size = 1) +
-  # geom_hline(yintercept = 0.1, linetype = "dashed", color = "black") +
-  ylab("Exponential growth rate (/day)") + 
-  xlab("Phosphate concentration (uM)") 
 
 ## bootstrapping from rtpc trial 3
 install.packages("minpack.lm")
+install.packages('nlstools')
+library(nlstools)
 library(minpack.lm)
 
-class(fit_model2) # this is a nls model 
+##file names all in one
+fit_model2 <- growth_sum_p2 %>%
+  mutate(r_concentration = as.numeric(r_concentration)) 
+  
+fit_model3 <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+      data = fit_model2,
+      start = list(ks = 0.5, umax = 0.5),
+      algorithm = "port",
+      control = nls.control(maxiter = 500, minFactor = 1/204800000))
 
+class(fit_model3) # this is a nls model 
+summary(fit_model3)
+
+boot1 <- nlsBoot(fit_model3)
+
+
+#mean / standard error for fist 8C
+boot1$estiboot
+#boot2 <- data_frame(boot1$estiboot, colnames(c("Estimate", "Std_error")))
+
+#median and 2.5 - 97.5 confidence intervals 
+boot1$bootCI 
+#%>% ggplot(aes(x ))
+
+##doing individually
+f_21C <- growth_sum_p2 %>%
+  filter(file_name == "Fist_21C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+f21C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+                  data = f_21C,
+                  start = list(ks = 0.5, umax = 0.5),
+                  algorithm = "port",
+                  control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_f21C <- nlsBoot(f21C_mod)
+b_f21C$bootCI
+
+f_8C <- growth_sum_p2 %>%
+  filter(file_name == "Fist_8C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+f8C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+                data = f_8C,
+                start = list(ks = 0.5, umax = 0.5),
+                algorithm = "port",
+                control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_f8C <- nlsBoot(f8C_mod)
+b_f8C$bootCI
+
+f_30C <- growth_sum_p2 %>%
+  filter(file_name == "Fist_30C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+f30C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+                data = f_30C,
+                start = list(ks = 0.5, umax = 0.5),
+                algorithm = "port",
+                control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_f30C <- nlsBoot(f30C_mod)
+b_f30C$bootCI
+
+s_21C <- growth_sum_p2 %>%
+  filter(file_name == "Scen_21C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+s21C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+                data = s_21C,
+                start = list(ks = 0.5, umax = 0.5),
+                algorithm = "port",
+                control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_s21C <- nlsBoot(s21C_mod)
+b_s21C$bootCI
+
+s_8C <- growth_sum_p2 %>%
+  filter(file_name == "Scen_8C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+s8C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+                data = s_8C,
+                start = list(ks = 0.5, umax = 0.5),
+                algorithm = "port",
+                control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_s8C <- nlsBoot(s8C_mod)
+b_s8C$bootCI
+
+s_30C <- growth_sum_p2 %>%
+  filter(file_name == "Scen_30C") %>% 
+  mutate(r_concentration = as.numeric(r_concentration)) 
+s30C_mod <- nls(mu ~ umax * (r_concentration / (ks + r_concentration)),
+               data = s_30C,
+               start = list(ks = 0.5, umax = 0.5),
+               algorithm = "port",
+               control = nls.control(maxiter = 500, minFactor = 1/204800000))
+b_s30C <- nlsBoot(s30C_mod)
+b_s30C$bootCI
+
+## make the dataframe of them all together and then assign a file name?
+f_21C_df <- data.frame(File_Name = "Fist_21C", CI = b_f21C$bootCI)
+f_8C_df <- data.frame(File_Name = "Fist_8C", CI = b_f8C$bootCI)
+f_30C_df <- data.frame(File_Name = "Fist_30C", CI = b_f30C$bootCI)
+s_21C_df <- data.frame(File_Name = "Scen_21C", CI = b_s21C$bootCI)
+s_8C_df <- data.frame(File_Name = "Scen_8C", CI = b_s8C$bootCI)
+s_30C_df <- data.frame(File_Name = "Scen_30C", CI = b_s30C$bootCI)
+
+# Combine dataframes into one
+all_bootCI_df <- rbind(f_21C_df, f_8C_df, f_30C_df, s_21C_df, s_8C_df, s_30C_df)
+## first column how do i remove?
+
+#estimate of error in fit of curve - confidence intervals use to compare between treatments - plot them and if standard errors overlap or not - 
+#indistingusihable if they do overlap  , different from one another if they dont overlap 
+#two plots, one is ks and one is umax 
+
+## how to separate that first row - add colours - coloumn that says parameters to separate by 
+all_bootCI_df %>%
+  ggplot(aes(x = File_Name, y = CI.Median)) +
+  #ggplot(aes(x = File_Name, y = CI.Median, colour = parameters)) +
+  geom_point(size = 4) +
+  #facet_wrap(~parameters) +
+  geom_errorbar(aes(ymin= CI.2.5., ymax= CI.97.5.), width=.2) +
+  labs(x = 'estimate',
+       y = 'treatment') +
+  scale_color_manual(values = c(ks = "#9e0142", umax = "sienna1"))
+
+# c("#9e0142","sienna1","#fbcf51","#4bc425","#3288bd","#5e4fa2")
+
+## trying a different bootstrapping method --------
 bootstrap_nls <- function(data, n_bootstraps) {
   bootstrapped_coefs <- replicate(1000, {
     boot_data <- preds4[sample(nrow(preds4), replace = TRUE), ]
