d4f37aeccf31fafed18362f7b7ded2059c171ba2
INwafor <inwafor@uoguelph.ca>
2024-07-29 16:45:09 -0400

    adding text from other scripts

--- code/10_july_tests.R
+++ code/10_july_tests.R
@@ -13,3 +13,17 @@ library(fs)
 
 ##starting with static vs shaking data 
 #import folder and files 
+
+platemap<- read_excel("Input_RFU/Acclimation_Trial_8d_Plates/platemap/plate_map.xlsx", col_names = T)
+platemaplong<- platemap %>% gather(2:13, key="Column", value = "ID")
+platemapwithcellid<- platemaplong %>%  unite (1:2,sep= "", col= "position" )
+
+raw<- list.files(path = "Input_RFU/Acclimation_Trial_8d_Plates", pattern = "xlsx")
+
+RFU_files2 <- c(list.files("data/rstar2_dataraw", full.names = TRUE))
+
+RFU_files2 <- RFU_files2[grepl(".xls", RFU_files2)]
+
+names(RFU_files2) <- RFU_files2 %>% 
+  gsub(pattern = ".xlsx$", replacement = "") %>% 
+  gsub(pattern = ".xls$", replacement = "")
