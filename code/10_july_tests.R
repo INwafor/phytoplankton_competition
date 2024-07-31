efd7b395beb145f815fe7bfc0e7c9566c157a75f
INwafor <inwafor@uoguelph.ca>
2024-07-31 16:18:48 -0400

    adding plate map

--- code/10_july_tests.R
+++ code/10_july_tests.R
@@ -13,27 +13,37 @@ library(fs)
 
 ##starting with static vs shaking data 
 #amandas plate map input 
-platemap<- read_excel("data/plate_map.xlsx", col_names = T)
-platemaplong<- platemap %>% gather(2:13, key="Column", value = "ID")
+tpc_platemap<- read_excel("data/plate_map.xlsx", col_names = T)
+platemaplong<- tpc_platemap %>% gather(2:13, key="Column", value = "ID")
 platemapwithcellid<- platemaplong %>%  unite (1:2,sep= "", col= "position" )
 
 #static data
 s_raw<- list.files(path = "data-raw/static-shaking", pattern = "xlsx")
 
 # need to do time manually - next time redo and download the way Amanda does + without template on protocol
+ss_template <- read_excel("data/plate_template.xlsx", sheet = "shake-static")
+ss_template <- ss_template[!(row.names(ss_template) %in% c("1")),]
 
-all_plates <- map_df(s_raw, read_excel, range = "A1:M9")
+s_files <- c(list.files("data-raw/static-shaking", full.names = TRUE))
 
+s_files <- s_files[grepl(".xls", s_files)]
 
-file.l
+names(s_files) <- s_files %>% 
+  gsub(pattern = ".xlsx$", replacement = "") %>% 
+  gsub(pattern = ".xls$", replacement = "")
 
-
+all_plates <- map_df(s_files, read_excel, .id = "file_name")%>%
   rename(row = ...1) %>% 
-  mutate(file_name = str_replace(file_name, " ", "")) %>%
-  separate(file_name, into = c("data", "location", "loc_2", "file_name", "temp", "read"), remove = FALSE)%>%
-  select(-data,
-         -location,
-         -loc_2)
+  mutate(file_name = str_replace(file_name, " ", "")) %>% 
+  separate(file_name, into = c("location", "loc_2","s1", "s2", "file_name", "read"), remove = FALSE)%>%
+  select(-location,
+         -loc_2, 
+         -s1,
+         -s2,
+         -...14)
+
+#all_merged2 <- left_join(all_temp_RFU, plate_layout, by = "well")
+
 
 all_plates <- unite(all_plates, file_name, c(file_name, temp, read))
 
